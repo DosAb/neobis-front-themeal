d7137b1506123cb7538cca949451bfaa2208fdaf
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-06-06 03:10:50 +0600

    fix bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://www.themealdb.com/api/json/v1/1/',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    baseURL: 'https://www.themealdb.com/api/json/v1/1/'
 })
 
 export const getRandomMeal = () => instance.get('random.php')
